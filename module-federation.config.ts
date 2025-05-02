976e09cb238b0d418a7a22ea193218ebfe767a92
Andre Camara <andre.m.camara@hotmail.com>
2025-05-02 02:40:35 -0300

    Adding config to federation module

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -3,10 +3,11 @@ import { createModuleFederationConfig } from '@module-federation/rsbuild-plugin'
 export default createModuleFederationConfig({
   name: 'fiap_mf_main',
   exposes: {
-    '.': './src/components/MainPage/MainPage.tsx',
+    './MainPage': './src/components/MainPage/MainPage.tsx',
   },
   shared: {
     react: { singleton: true },
     'react-dom': { singleton: true },
   },
+  getPublicPath: 'http://localhost:3001/',
 });
